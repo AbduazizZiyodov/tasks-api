b711431206df0825ae94c54f1c255dcdd359d6c9
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-10-03 15:07:49 +0500

    :fire: Added `redoc`!

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,16 @@ urlpatterns: list = [
         r'^$', schema_view.with_ui(
             'swagger',
             cache_timeout=0
-        ), 
+        ),
         name='schema-swagger-ui'
     ),
+
+    url(
+        r'^redoc', schema_view.with_ui(
+            'redoc',
+            cache_timeout=0
+        ),
+        name='schema-redoc'
+    ),
+
 ]
--- core/yasg.py
+++ core/yasg.py
@@ -6,8 +6,15 @@ from drf_yasg.views import get_schema_view
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Tasks API",
+        title="Tasks-API",
         default_version="v1",
+        description="Simple API for managing tasks written DRF",
+        contact=openapi.Contact(
+            "Abduaziz Ziyodov",
+            "https://github.com/AbduazizZiyodov",
+            "abduaziz.ziyodov@mail.ru"
+        ),
+        license=openapi.License("MIT")
     ),
     public=True,
     permission_classes=(AllowAny,),
