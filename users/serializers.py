ac4e37a003aae4aaaeb67f4d023f0c1a2535c8fa
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-10-15 22:42:32 +0500

    :fire: Improved User serializer: group names instead of their ids

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,22 @@
+from typing import List
+
 from rest_framework import serializers
+
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
+    groups = serializers.SerializerMethodField()
+
+    def get_groups(self, obj: User) -> List[str]:
+        return [
+            group.name
+            for group in obj.groups.all()
+        ]
+
     class Meta:
         model = get_user_model()
         fields = '__all__'
